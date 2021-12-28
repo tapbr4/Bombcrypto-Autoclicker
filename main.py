d66984933c29523b530e1aa6f22c9e1ad35c7cb1
Tiago Araújo <tiagoapx6@outlook.com>
2021-12-27 22:41:27 -0300

    Timestamps added to logs; Bugfix in bombers idle prevention script.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ast import literal_eval as make_tuple
+from datetime import datetime
 from time import sleep
 import pyautogui
 
@@ -32,7 +33,7 @@ sleep(3)
 
 ## Loop
 while True:
-    print('\n[STARTING LOOP]')
+    print('\n[STARTING LOOP]\nLOGGING INTO THE GAME [{}]'.format(str(datetime.now().hour) + ':' + str(datetime.now().minute)))
     ## Entering the Website
     pyautogui.click(x=pos['address_bar'][0], y=pos['address_bar'][1])
     sleep(3)
@@ -43,7 +44,6 @@ while True:
     sleep(6)
 
     ## Play / Metamask Confirmation
-    print('PLAY NOW!')
     pyautogui.click(x=pos['play_now'][0], y=pos['play_now'][1])
     sleep(10)
     pyautogui.hotkey('alt', 'tab')
@@ -58,7 +58,7 @@ while True:
     sleep(30)
 
     ## In-game / Work
-    print('PUTTING TO WORK')
+    print('PUTTING TO WORK [{}]'.format(str(datetime.now().hour) + ':' + str(datetime.now().minute)))
     pyautogui.click(x=pos['treasure_hunt'][0], y=pos['treasure_hunt'][1])
     sleep(5)
     pyautogui.click(x=pos['map'][0], y=pos['map'][1], clicks=2, interval=0.5)
@@ -81,7 +81,7 @@ while True:
 
     ## Working 1h (+1minute and 6 seconds)
     ## Click routine to go to the next map and avoid error messages.
-    print('1 HOUR FARM')
+    print('WORKING 1H [{}]'.format(str(datetime.now().hour) + ':' + str(datetime.now().minute)))
     pyautogui.click(x=pos['map'][0], y=pos['map'][1], clicks=2, interval=0.5)
     for i in range(60): 
         sleep(15)
@@ -96,14 +96,14 @@ while True:
         pyautogui.click(x=pos['treasure_hunt'][0], y=pos['treasure_hunt'][1])
         ### Prevents bombers from walking around without placing bombs.
         if(i%5==0 and i!=0): # Runs 11 times. (+1 minute and 6 seconds)
-            sleep(3)
+            sleep(2)
             pyautogui.click(x=pos['main_menu'][0], y=pos['main_menu'][1])
-            sleep(3)
-            pyautogui.click(x=pos['treasure_hunt'][0], y=pos['treasure_hunt'][1])
+            sleep(2)
+            pyautogui.click(x=pos['treasure_hunt'][0], y=pos['treasure_hunt'][1], clicks=2, interval=2)
     sleep(5)
 
     ## Rest
-    print('PUTTING TO REST')
+    print('PUTTING TO REST [{}]'.format(str(datetime.now().hour) + ':' + str(datetime.now().minute)))
     pyautogui.click(x=pos['menu_arrow'][0], y=pos['menu_arrow'][1])
     sleep(5)
     pyautogui.click(x=pos['heroes'][0], y=pos['heroes'][1])
@@ -136,5 +136,5 @@ while True:
     pyautogui.hotkey('ctrl', 'w')
 
     ## Resting 1h (+1h farm = 2h frequency)
-    print('REST 1H')
+    print('RESTING 1H [{}]'.format(str(datetime.now().hour) + ':' + str(datetime.now().minute)))
     sleep(3600) # 1h = 3600s
