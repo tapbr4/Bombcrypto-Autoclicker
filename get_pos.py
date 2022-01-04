7c3e75b2fbd5380849bbccf9297b7bd28ead9a2c
Tiago Araújo <tiagoapx6@outlook.com>
2022-01-04 09:40:15 -0300

    Now works with Multi-Account

--- get_pos.py
+++ get_pos.py
@@ -1,10 +1,15 @@
-# Command to run in background (Windows Powershell):
+# Run after opening your Browser and logging into Metamask !!!
+
+# If you are using Windows Power Toys + Multiple browsers just run the script
+# Otherwise, run the following command in Windows Powershell (Without #):
 # Start-Process -NoNewWindow python get_pos.py
+
 from pynput import mouse
 
 
-print('Running...')
-# After opening your Browser and logging into Metamask
+accounts = 2 #Enter the number of accounts/browsers here
+browser = 1 #Just a counter. Keep the value 1!
+
 name_list = [
     'Browser Icon (Taskbar)', 'Address Bar', '"Play Now!" Button', 
     '"Connect Wallet" Button', 'Metamask Icon (Taskbar)', 'Metamask Confirmation', 
@@ -14,17 +19,26 @@ name_list = [
 lines = []
 count = 0
 
+print('Running...')
+
 def on_click(x, y, button, pressed):
+    global accounts
+    global browser
     global count
+
     if button == mouse.Button.left and pressed:
         lines.append('{}'.format((x, y)))
-        print('{} at {}'.format(name_list[count], (x, y)))
+        print('[Browser {}] {} at {}'.format(browser, name_list[count], (x, y)))
         count += 1
         
     if count == len(name_list):
+        browser += 1
+        count = 0
+
+    if browser > accounts:
         with open('config.txt', 'w') as f:
             f.write('\n'.join(lines))
-        
+
         print('Finished!')
         return False
 
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pyautogui
 
 
 # Setup #
+browsers = 2 #Enter the number of accounts/browsers here
 name_list = [
     'browser_icon','address_bar',
     'play_now', 'connect_wallet',
@@ -13,128 +14,143 @@ name_list = [
     'heroes','character_list',
     'work','rest','close',
     'map','next_map','main_menu']
-pos = {}
+pos = []
 
 # Read config.txt #
 with open('config.txt') as f:
     lines = f.read().splitlines()
 
 # Dict #
-count = 0
-for item in lines:
-    t_item = make_tuple(item)
-    pos[name_list[count]] = t_item
-    count += 1
+line = 0
+for browser in range(1, browsers+1):
+    pos.append(dict())
+    count = 0
+    for item in lines[line:(browser*16)]:
+        t_item = make_tuple(item)
+        pos[browser-1][name_list[count]] = t_item
+        count += 1
+    line += 16
 
 # Auto #
 ## Opening Browser
-pyautogui.click(x=pos['browser_icon'][0], y=pos['browser_icon'][1])
+for i in range(browsers):
+    pyautogui.click(x=pos[i]['browser_icon'][0], y=pos[i]['browser_icon'][1])
 sleep(3)
 
 ## Loop
 while True:
     print('\n[STARTING LOOP]\nLOGGING INTO THE GAME [{}]'.format(str(datetime.now().hour) + ':' + str(datetime.now().minute)))
     ## Entering the Website
-    pyautogui.click(x=pos['address_bar'][0], y=pos['address_bar'][1])
-    sleep(3)
-    pyautogui.write('https://bombcrypto.io/')   
-    pyautogui.press('enter')
-    sleep(3)
-    pyautogui.press('enter')
-    sleep(6)
+    for i in range(browsers):
+        pyautogui.click(x=pos[i]['address_bar'][0], y=pos[i]['address_bar'][1])
+        sleep(2)
+        pyautogui.write('https://bombcrypto.io/')   
+        pyautogui.press('enter')
+        sleep(2)
+        pyautogui.press('enter')
+        sleep(2)
 
-    ## Play / Metamask Confirmation
-    pyautogui.click(x=pos['play_now'][0], y=pos['play_now'][1])
-    sleep(10)
-    pyautogui.hotkey('alt', 'tab')
-    sleep(3)
-    pyautogui.hotkey('ctrl', 'w')
-    sleep(5)
-    pyautogui.click(x=pos['connect_wallet'][0], y=pos['connect_wallet'][1])
-    sleep(20)
-    pyautogui.click(x=pos['metamask_icon'][0], y=pos['metamask_icon'][1], clicks=2, interval=0.25)
-    sleep(5)
-    pyautogui.click(x=pos['metamask_confirm'][0], y=pos['metamask_confirm'][1])
-    sleep(30)
+        ## Play / Metamask Confirmation
+        pyautogui.click(x=pos[i]['play_now'][0], y=pos[i]['play_now'][1])
+        sleep(5)
+        pyautogui.hotkey('alt', 'tab')
+        sleep(3)
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(5)
+        pyautogui.click(x=pos[i]['connect_wallet'][0], y=pos[i]['connect_wallet'][1])
+        sleep(15)
+        pyautogui.click(x=pos[i]['metamask_icon'][0], y=pos[i]['metamask_icon'][1], clicks=2, interval=0.25)
+        sleep(5)
+        pyautogui.click(x=pos[i]['metamask_confirm'][0], y=pos[i]['metamask_confirm'][1])
+        sleep(5)
 
     ## In-game / Work
     print('PUTTING TO WORK [{}]'.format(str(datetime.now().hour) + ':' + str(datetime.now().minute)))
-    pyautogui.click(x=pos['treasure_hunt'][0], y=pos['treasure_hunt'][1])
-    sleep(5)
-    pyautogui.click(x=pos['map'][0], y=pos['map'][1], clicks=2, interval=0.5)
-    sleep(5)
-    pyautogui.click(x=pos['menu_arrow'][0], y=pos['menu_arrow'][1])
-    sleep(5)
-    pyautogui.click(x=pos['heroes'][0], y=pos['heroes'][1])
-    sleep(5)
-    pyautogui.click(x=pos['character_list'][0], y=pos['character_list'][1])
-    sleep(4)
-    for i in range(45):
-        pyautogui.scroll(-1)
-    sleep(8)
-    for i in range(16):
-        pyautogui.click(x=pos['work'][0], y=pos['work'][1])
+    for i in range(browsers):
+        pyautogui.click(x=pos[i]['treasure_hunt'][0], y=pos[i]['treasure_hunt'][1])
+        sleep(5)
+        pyautogui.click(x=pos[i]['map'][0], y=pos[i]['map'][1], clicks=2, interval=0.5)
+        sleep(5)
+        pyautogui.click(x=pos[i]['menu_arrow'][0], y=pos[i]['menu_arrow'][1])
+        sleep(5)
+        pyautogui.click(x=pos[i]['heroes'][0], y=pos[i]['heroes'][1])
+        sleep(5)
+        pyautogui.click(x=pos[i]['character_list'][0], y=pos[i]['character_list'][1])
         sleep(3)
-    sleep(2)
-    pyautogui.click(x=pos['close'][0], y=pos['close'][1], clicks=2, interval=1.5)
-    sleep(5)
+        for j in range(45):
+            pyautogui.scroll(-1)
+        sleep(3)
+        for j in range(16):
+            pyautogui.click(x=pos[i]['work'][0], y=pos[i]['work'][1])
+            sleep(3)
+        sleep(2)
+        pyautogui.click(x=pos[i]['close'][0], y=pos[i]['close'][1], clicks=2, interval=1.5)
+        sleep(5)
+        pyautogui.click(x=pos[i]['map'][0], y=pos[i]['map'][1], clicks=2, interval=0.5)
+        sleep(5)
 
-    ## Working 1h (+1minute and 6 seconds)
+    ## Working 1h (+Some sleep based on number of accounts)
     ## Click routine to go to the next map and avoid error messages.
     print('WORKING 1H [{}]'.format(str(datetime.now().hour) + ':' + str(datetime.now().minute)))
-    pyautogui.click(x=pos['map'][0], y=pos['map'][1], clicks=2, interval=0.5)
-    for i in range(60): 
+    for i in range(60):
         sleep(15)
-        pyautogui.click(x=pos['next_map'][0], y=pos['next_map'][1])
-        sleep(10)
-        pyautogui.click(x=pos['connect_wallet'][0], y=pos['connect_wallet'][1])
+        for j in range(browsers):
+            pyautogui.click(x=pos[j]['next_map'][0], y=pos[j]['next_map'][1])
         sleep(15)
-        pyautogui.click(x=pos['metamask_icon'][0], y=pos['metamask_icon'][1], clicks=2, interval=0.25)
-        sleep(5)
-        pyautogui.click(x=pos['metamask_confirm'][0], y=pos['metamask_confirm'][1])
+        for j in range(browsers):
+            pyautogui.click(x=pos[j]['connect_wallet'][0], y=pos[j]['connect_wallet'][1])
         sleep(15)
-        pyautogui.click(x=pos['treasure_hunt'][0], y=pos['treasure_hunt'][1])
+        for j in range(browsers):
+            pyautogui.click(x=pos[j]['metamask_icon'][0], y=pos[j]['metamask_icon'][1], clicks=2, interval=0.25)
+            sleep(1)
+            pyautogui.click(x=pos[j]['metamask_confirm'][0], y=pos[j]['metamask_confirm'][1], clicks=2, interval=0.25)
+        sleep(15)
+        for j in range(browsers):
+            pyautogui.click(x=pos[j]['treasure_hunt'][0], y=pos[j]['treasure_hunt'][1])
         ### Prevents bombers from walking around without placing bombs.
         if(i%5==0 and i!=0): # Runs 11 times. (+1 minute and 6 seconds)
             sleep(2)
-            pyautogui.click(x=pos['main_menu'][0], y=pos['main_menu'][1])
+            for j in range(browsers):
+                pyautogui.click(x=pos[j]['main_menu'][0], y=pos[j]['main_menu'][1])
             sleep(2)
-            pyautogui.click(x=pos['treasure_hunt'][0], y=pos['treasure_hunt'][1], clicks=2, interval=2)
+            for j in range(browsers):
+                pyautogui.click(x=pos[j]['treasure_hunt'][0], y=pos[j]['treasure_hunt'][1], clicks=2, interval=2)
     sleep(5)
 
     ## Rest
     print('PUTTING TO REST [{}]'.format(str(datetime.now().hour) + ':' + str(datetime.now().minute)))
-    pyautogui.click(x=pos['menu_arrow'][0], y=pos['menu_arrow'][1])
-    sleep(5)
-    pyautogui.click(x=pos['heroes'][0], y=pos['heroes'][1])
-    sleep(5)
-    pyautogui.click(x=pos['character_list'][0], y=pos['character_list'][1])
-    sleep(4)
-    for i in range(45):
-        pyautogui.scroll(1)
-    sleep(8)
-    for i in range(16):
-        pyautogui.click(x=pos['rest'][0], y=pos['rest'][1])
+    for i in range(browsers):
+        pyautogui.click(x=pos[i]['menu_arrow'][0], y=pos[i]['menu_arrow'][1])
+        sleep(5)
+        pyautogui.click(x=pos[i]['heroes'][0], y=pos[i]['heroes'][1])
+        sleep(5)
+        pyautogui.click(x=pos[i]['character_list'][0], y=pos[i]['character_list'][1])
         sleep(3)
-    sleep(2)
-    pyautogui.click(x=pos['close'][0], y=pos['close'][1], clicks=2, interval=1.5)
-    sleep(5)
-    pyautogui.click(x=pos['map'][0], y=pos['map'][1], clicks=2, interval=0.5)
-    sleep(5)
-    
-    ## Rest Confirmation
-    pyautogui.click(x=pos['main_menu'][0], y=pos['main_menu'][1])
-    sleep(5)
-    pyautogui.click(x=pos['treasure_hunt'][0], y=pos['treasure_hunt'][1])
-    sleep(5)
+        for j in range(45):
+            pyautogui.scroll(1)
+        sleep(3)
+        for j in range(16):
+            pyautogui.click(x=pos[i]['rest'][0], y=pos[i]['rest'][1])
+            sleep(3)
+        sleep(2)
+        pyautogui.click(x=pos[i]['close'][0], y=pos[i]['close'][1], clicks=2, interval=1.5)
+        sleep(5)
+        pyautogui.click(x=pos[i]['map'][0], y=pos[i]['map'][1], clicks=2, interval=0.5)
+        sleep(5)
+        
+        ## Rest Confirmation
+        pyautogui.click(x=pos[i]['main_menu'][0], y=pos[i]['main_menu'][1])
+        sleep(3)
+        pyautogui.click(x=pos[i]['treasure_hunt'][0], y=pos[i]['treasure_hunt'][1])
+        sleep(3)
 
-    ## Close Tab
-    pyautogui.hotkey('ctrl', 't')
-    sleep(3)
-    pyautogui.hotkey('alt', 'tab')
-    sleep(2)
-    pyautogui.hotkey('ctrl', 'w')
+        ## Close Tab
+        pyautogui.hotkey('ctrl', 't')
+        sleep(3)
+        pyautogui.hotkey('alt', 'tab')
+        sleep(3)
+        pyautogui.hotkey('ctrl', 'w')
 
     ## Resting 1h (+1h farm = 2h frequency)
     print('RESTING 1H [{}]'.format(str(datetime.now().hour) + ':' + str(datetime.now().minute)))
-    sleep(3600) # 1h = 3600s
+    sleep(3600-(85*browsers)) # 1h = 3600s | 85s = Total Sleep (Putting to Rest)
